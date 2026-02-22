cf2366ad2e96c4b0c863e9d77432f5ec2a5803dc
shilpijat09-hash <shilpijat09@gmail.com>
2026-02-22 09:43:25 +0530

    Notes

--- Leetcode/1_Sum_of_Two_Number.cpp
+++ Leetcode/1_Sum_of_Two_Number.cpp
@@ -1,17 +1,19 @@
-#include<iostream>
+#include <iostream>
 #include<vector>
 using namespace std;
-int main(){
-    int n
-}
-        for(int i=0;i<=nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
+vector<int> twoSum(vector<int>& nums, int target) {
+    for(int i=0;i<nums.size();i++){
+        for(int j=i+1;j<nums.size();j++){
+            if(nums[i]+nums[j]==target){
+                return {i,j};
             }
-            
-            }
-            return{};
         }
+    }
+    return {};
+}
+int main() {
+    vector<int> nums={2,7,11,15};
+    int target =9;
+    vector<int> a=twoSum(nums,target);
+    return 0;
 }
