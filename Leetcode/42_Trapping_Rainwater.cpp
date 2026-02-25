5dec258d48279917557f9e2a623168f07090420a
shilpijat09-hash <shilpijat09@gmail.com>
2026-02-25 15:41:02 +0530

    Notes

---
+++ Leetcode/42_Trapping_Rainwater.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+int trap(vector<int>& height) {
+    int n = height.size();
+    int left = 0 , right = n-1;
+    int answer = 0;
+    int rightmax = 0 , leftmax = 0;
+
+    while (left < right){
+if(height[left]<=height[right]){
+    if(leftmax>=height[left]){
+        answer+=leftmax-height[left];
+    }
+    else{
+        leftmax=height[left];
+    }
+    left++;
+}
+else{
+    if(rightmax>=height[right]){
+        answer+=rightmax-height[right];
+    }
+    else{
+        rightmax=height[right];
+    }
+    right--;
+}    
+}
+return answer;
+}
+int main() {
+    
+    return 0;
+}
+//  int trap(vector<int>& height) {
+//        int n = height.size();
+//     int left = 0 , right = n-1;
+//     int answer = 0;
+//     int rightmax = 0 , leftmax = 0;
+
+//     while (left < right){
+
+//         leftmax = max(leftmax , height[left]);
+//         rightmax = max(rightmax , height[right]);
+
+//         if(leftmax < rightmax){
+
+//             answer += leftmax - height[left];
+//             left++;
+//         }
+//         else{
+//             answer += rightmax - height[right];
+//             right--;
+//         }       
+//     }
+//     return answer;
+// }
+// int main() {
+    
+//     return 0;  
+//     }
+// };
---
+++ Leetcode/tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+int trap(vector<int>& height) {
+//        int n = height.size();
+//     int left = 0 , right = n-1;
+//     int answer = 0;
+//     int rightmax = 0 , leftmax = 0;
+
+//     while (left < right){
+
+//         leftmax = max(leftmax , height[left]);
+//         rightmax = max(rightmax , height[right]);
+
+//         if(leftmax < rightmax){
+
+//             answer += leftmax - height[left];
+//             left++;
+//         }
+//         else{
+//             answer += rightmax - height[right];
+//             right--;
+//         }       
+//     }
+//     return answer;
+// }
+// int main() {
+    
+//     return 0;  
+//     }
+// };
