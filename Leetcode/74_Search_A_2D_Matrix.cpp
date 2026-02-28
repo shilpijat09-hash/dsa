cf7468fa9e71fbb97500d37ebec67b778053fb4d
shilpijat09-hash <shilpijat09@gmail.com>
2026-02-28 14:26:04 +0530

    Notes

---
+++ Leetcode/74_Search_A_2D_Matrix.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int  i = 0, j = matrix[0].size()-1;
+        while (i<matrix.size() && j>=0){
+            if (matrix[i][j] == target) return true;
+            else if (target>matrix[i][j]) i+=1;
+            else if (target<matrix[i][j]) j-= 1;
+
+        }
+        return false;
+    }
