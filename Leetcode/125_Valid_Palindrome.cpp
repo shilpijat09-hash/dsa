817c9d871d77a67d04e25a9cd40f3616838f8c65
shilpijat09-hash <shilpijat09@gmail.com>
2026-03-14 12:39:56 +0530

    Notes

--- Leetcode/125_Valid_Palindrome.cpp
+++ Leetcode/125_Valid_Palindrome.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
-#include<vector>
+#include <string>
+#include <cctype>
 using namespace std;
-    bool isPalindrome(string s) {
-        int l=0;
-        int r=s.size()-1;
-        while(l<r){
-            while(l<r && !isalnum(s[l])){
-                l++;
-            }
-            while(l<r && !isalnum(s[r])){
-                r--;
-            }
-            if(tolower(s[l])!=tolower(s[r])){
-                return false;
-            }
+
+bool isPalindrome(string s) {
+    int l = 0;
+    int r = s.size() - 1;
+
+    while(l < r){
+        while(l < r && !isalnum(s[l])){
             l++;
+        }
+        while(l < r && !isalnum(s[r])){
             r--;
         }
-        return true;
+
+        if(tolower(s[l]) != tolower(s[r])){
+            return false;
+        }
+
+        l++;
+        r--;
     }
+
+    return true;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    if(isPalindrome(s)){
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+
+    return 0;
+}
