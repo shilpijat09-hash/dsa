656c83297ad6f1d27a4b085d956bc906b29bf679
shilpijat09-hash <shilpijat09@gmail.com>
2026-03-14 12:29:28 +0530

    Notes

--- Leetcode/389_Find_The_Difference.cpp
+++ Leetcode/389_Find_The_Difference.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
-#include<vector>
+#include <string>
 using namespace std;
-    char findTheDifference(string s, string t) {
-        char se;
-        int freq[26]={0};
-        for(int i=0;i<t.size();i++){
-            freq[t[i]-'a']++;
-        }
-        for(int i=0;i<s.size();i++){
-            freq[s[i]-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(freq[i]==1){
-                se='a'+i;
-            }
-        }
-        return se;
+
+char findTheDifference(string s, string t) {
+    char se;
+    int freq[26] = {0};
+
+    for(int i = 0; i < t.size(); i++){
+        freq[t[i] - 'a']++;
     }
+
+    for(int i = 0; i < s.size(); i++){
+        freq[s[i] - 'a']--;
+    }
+
+    for(int i = 0; i < 26; i++){
+        if(freq[i] == 1){
+            se = 'a' + i;
+        }
+    }
+
+    return se;
+}
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    cout << findTheDifference(s, t);
+
+    return 0;
+}
