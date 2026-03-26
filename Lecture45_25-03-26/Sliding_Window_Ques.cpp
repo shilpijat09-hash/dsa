6c108b2b5154c29f2418cfb2fd43b862ea67b028
shilpijat09-hash <shilpijat09@gmail.com>
2026-03-26 13:35:18 +0530

    Notes

--- Lecture45_25-03-26/Sliding_Window_Ques.cpp
+++ Lecture45_25-03-26/Sliding_Window_Ques.cpp
@@ -1,6 +1,39 @@
-//leetcode ques 643 maximum average subarray I
-#include<iostream>
+//leetcode ques 1456 maximum number of vowels in a substring of given length
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int main(){
-    
+bool isVowel(char c){
+    return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+}
+
+int maxVowels(string s, int k) {
+    int n = s.size();
+    int l = 0, r = 0;
+    int count = 0, ans = 0;
+
+    while(r < n){
+        // current character ko add kara hai 
+        if(isVowel(s[r])) count++;
+
+        // container size maintain karni hai
+        if(r - l + 1 > k){
+            if(isVowel(s[l])) count--;
+            l++;
+        }
+        // jab meri container size k ke equal ho jaaye
+        if(r - l + 1 == k){
+            ans = max(ans, count);
+        }
+
+        r++;
+    }
+
+    return ans;
+}
+int main() {
+    string s="abciiidef";
+    int k=3;
+    cout<<maxVowels(s,k)<<endl;
+    return 0;
 }
