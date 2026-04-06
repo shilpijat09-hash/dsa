6a2f9023e683fe739dd6deaba2e891b5f712c15c
shilpijat09-hash <shilpijat09@gmail.com>
2026-04-06 23:00:37 +0530

    Notes

--- Leetcode/118_Pascal's_Triangle.cpp
+++ Leetcode/118_Pascal's_Triangle.cpp
@@ -1,7 +1,6 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
-  vector<vector<int>> generate(int numRows) {
+vector<vector<int>> generate(int numRows) {
     vector<vector<int>> ans;
 
     for (int i = 0; i < numRows; ++i)
@@ -12,4 +11,34 @@ using namespace std;
         ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
 
     return ans;
-  }
+}
+int main() {
+    int numRows = 5;
+    vector<vector<int>> result = generate(numRows);
+    for(auto row : result) {
+        for(int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+=======
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < numRows; ++i)
+      ans.push_back(vector<int>(i + 1, 1));
+
+    for (int i = 2; i < numRows; ++i)
+      for (int j = 1; j < ans[i].size() - 1; ++j)
+        ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+
+    return ans;
+}
+int main() {
+    
+    return 0;
+>>>>>>> 2169aa5ed0faf67289cc73db85a439fededbde77
+}
